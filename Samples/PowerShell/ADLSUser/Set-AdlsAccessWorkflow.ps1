5ab63f2da34680d5066f7f73320d748d3acad896
GRAHAM RUSSELL <graham.russell@microsoft.com>
2017-08-31 13:14:04 +0100

    check vars

--- Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
+++ Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
@@ -23,7 +23,8 @@ workflow adlsAccessSet {
 
 #$paths = "Clarity_20150312","Cloud_Telemetry_production","Emoji_iOS_beta","Emoji_iOS_market", "Hexy_prod", "SwiftKey_Android_beta", "SwiftKey_Android_beta_20150422", "SwiftKey_Android_beta_RS0.9", "SwiftKey_Android_emoji", "SwiftKey_Android_emoji_beta", "SwiftKey_Android_nn", "SwiftKey_Android_prod", "SwiftKey_Android_prod_20150422", "SwiftKey_Android_shakespeare", "SwiftKey_Android_touch_data", "SwiftKey_iOS_beta", "SwiftKey_iOS_prod", "deblois-android-release"
 $paths = "Clarity_20150312","Cloud_Telemetry_production"
-
+"$tenantid"
+"$subscriptionid"
 adlsAccessSet  -adlspaths $paths -wftenantid $tenantid -wfsubscriptionid $subscriptionid
 
 
