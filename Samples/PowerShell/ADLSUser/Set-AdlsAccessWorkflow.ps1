0f7a4dfd09872df060effecee22b0bbec2c9ec68
GRAHAM RUSSELL <graham.russell@microsoft.com>
2017-08-31 10:00:38 +0100

    ls folder above

--- Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
+++ Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
@@ -6,6 +6,7 @@ workflow adlsAccessSet {
     
         "$path"
         pwd
+        ls ..
         # InlineScript
         # {
         # .\Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/$Using:path/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
