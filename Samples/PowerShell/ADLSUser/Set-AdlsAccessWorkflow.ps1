1a104d1def6a156d3b16a1914741019dec627c59
GRAHAM RUSSELL <graham.russell@microsoft.com>
2017-08-31 14:55:20 +0100

    construct pscreds

--- Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
+++ Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
@@ -1,7 +1,20 @@
+param(
+    $tenantid,
+    $sp_username,
+    $sp_app_id,
+    $sp_password
+
+)
+
 workflow adlsAccessSet {
     
        param(
-           [string[]]$adlspaths
+           [string[]]$adlspaths,
+           [string]$tenantid,
+           [string]$sp_username,
+           [string]$sp_app_id,
+           [string]$sp_password
+
         )
     
        foreach -parallel ($path in $adlspaths){
@@ -9,15 +22,19 @@ workflow adlsAccessSet {
         InlineScript
         {
             "$path"
-            C:\Users\skadmin\agent\_work\1\s\Samples\PowerShell\ADLSUser\Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/SwiftKey_Android_prod/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
+            $userName =$sp_username
+            $password = ConvertTo-SecureString $sp_password -AsPlainText -Force
+            $credential = New-Object System.Management.Automation.PSCredential($userName, $password)
+            # Login-AzureRmAccount -ServicePrincipal -ApplicationId  $sp_app_id -Credential $credential -TenantId $tenantid
+            # C:\Users\skadmin\agent\_work\1\s\Samples\PowerShell\ADLSUser\Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/SwiftKey_Android_prod/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
             # .\Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/$Using:path/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
         }
         } 
     }
 
 #$paths = "Clarity_20150312","Cloud_Telemetry_production","Emoji_iOS_beta","Emoji_iOS_market", "Hexy_prod", "SwiftKey_Android_beta", "SwiftKey_Android_beta_20150422", "SwiftKey_Android_beta_RS0.9", "SwiftKey_Android_emoji", "SwiftKey_Android_emoji_beta", "SwiftKey_Android_nn", "SwiftKey_Android_prod", "SwiftKey_Android_prod_20150422", "SwiftKey_Android_shakespeare", "SwiftKey_Android_touch_data", "SwiftKey_iOS_beta", "SwiftKey_iOS_prod", "deblois-android-release"
-$paths = "Clarity_20150312","Cloud_Telemetry_production"
-adlsAccessSet  -adlspaths $paths
+$paths = "Clarity_20150312"
+adlsAccessSet  -adlspaths $paths -sp_username $sp_username -sp_app_id $sp_app_id -sp_app_pass $sp_app_pass -tenantid $tenantid
 
 
 #az dls fs list -n analyticstelemetry --path /app-telemetry/parquet_flat --output json | jq '.[].pathSuffix' -r
