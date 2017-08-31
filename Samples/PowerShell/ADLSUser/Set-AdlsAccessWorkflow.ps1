243a9516fe1fe64d84b67248151ce9e851d25bb5
GRAHAM RUSSELL <graham.russell@microsoft.com>
2017-08-31 13:34:54 +0100

    Try outside of inline script

--- Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
+++ Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
@@ -2,6 +2,9 @@ param(
     [string]$tenantid,
     [string]$subscriptionid
 )
+Import-Module -Name AzureRM -Global
+Import-Module -Name AzureRM.Profile -Global
+Add-AzureRMAccount -ServicePrincipal -TenantId $tenantid -Credential System.Management.Automation.PSCredential -Environment AzureCloud
 
 workflow adlsAccessSet {
     
@@ -15,9 +18,6 @@ workflow adlsAccessSet {
         "$path"
         InlineScript
         {
-            Import-Module -Name AzureRM -Global
-            Import-Module -Name AzureRM.Profile -Global
-            Add-AzureRMAccount -ServicePrincipal -TenantId $Using:wftenantid -Credential System.Management.Automation.PSCredential -Environment AzureCloud
             # Select-AzureRMSubscription -SubscriptionId $Using:subscriptionid -TenantId $Using:tenantid
             # .\Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/$Using:path/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
         }
@@ -26,6 +26,7 @@ workflow adlsAccessSet {
 
 #$paths = "Clarity_20150312","Cloud_Telemetry_production","Emoji_iOS_beta","Emoji_iOS_market", "Hexy_prod", "SwiftKey_Android_beta", "SwiftKey_Android_beta_20150422", "SwiftKey_Android_beta_RS0.9", "SwiftKey_Android_emoji", "SwiftKey_Android_emoji_beta", "SwiftKey_Android_nn", "SwiftKey_Android_prod", "SwiftKey_Android_prod_20150422", "SwiftKey_Android_shakespeare", "SwiftKey_Android_touch_data", "SwiftKey_iOS_beta", "SwiftKey_iOS_prod", "deblois-android-release"
 $paths = "Clarity_20150312","Cloud_Telemetry_production"
+$creds = System.Management.Automation.PSCredential
 adlsAccessSet  -adlspaths $paths -wftenantid $tenantid -wfsubscriptionid $subscriptionid
 
 
