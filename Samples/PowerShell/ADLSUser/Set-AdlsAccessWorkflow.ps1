2ec1277b128310a37ce20e462d0c11fee9754696
GRAHAM RUSSELL <graham.russell@microsoft.com>
2017-08-31 09:43:58 +0100

    remove inlinescript for now

--- Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
+++ Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
@@ -5,10 +5,10 @@ workflow adlsAccessSet {
        foreach –parallel ($path in $adlspaths){
     
         "$path"
-        InlineScript
-        {
-        .\Samples/PowerShell/ADLSUser/Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/$Using:path/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
-        }
+        # InlineScript
+        # {
+        # .\Samples/PowerShell/ADLSUser/Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/$Using:path/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
+        # }
         } 
     }
 
