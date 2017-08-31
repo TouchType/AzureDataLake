f8183dbbaf17f385c985c590557f823585faf575
GRAHAM RUSSELL <graham.russell@microsoft.com>
2017-08-31 15:05:07 +0100

    try login

--- Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
+++ Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
@@ -25,7 +25,7 @@ workflow adlsAccessSet {
             $userName =$Using:sp_username
             $password = ConvertTo-SecureString $Using:sp_password -AsPlainText -Force
             $credential = New-Object System.Management.Automation.PSCredential($userName, $password)
-            # Login-AzureRmAccount -ServicePrincipal -ApplicationId  $sp_app_id -Credential $credential -TenantId $tenantid
+            Login-AzureRmAccount -ServicePrincipal -ApplicationId  $Using:sp_app_id -Credential $credential -TenantId $Using:tenantid
             # C:\Users\skadmin\agent\_work\1\s\Samples\PowerShell\ADLSUser\Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/SwiftKey_Android_prod/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
             # .\Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/$Using:path/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
         }
