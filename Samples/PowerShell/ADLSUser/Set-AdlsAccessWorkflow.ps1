bab99b02512d42894d3b363ef636f37e24b7b9a0
GRAHAM RUSSELL <graham.russell@microsoft.com>
2017-08-31 10:13:44 +0100

    full paths

--- Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
+++ Samples/PowerShell/ADLSUser/Set-AdlsAccessWorkflow.ps1
@@ -5,11 +5,10 @@ workflow adlsAccessSet {
        foreach -parallel ($path in $adlspaths){
     
         "$path"
-        ls ps_modules
-        # InlineScript
-        # {
-        # .\Samples\PowerShell\ADLSUser\Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/$Using:path/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
-        # }
+        InlineScript
+        {
+        C:\Users\skadmin\agent\_work\1\s\Samples\PowerShell\ADLSUser\Set-AdlsAccess.ps1 -Account analyticstelemetry -Path /app-telemetry/parquet_flat/$Using:path/ -EntityId 609efa91-4471-4a8f-b421-fce5286fba17 -EntityType Group -SetAclEntry -Permissions ReadExecute
+        }
         } 
     }
 
